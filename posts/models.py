e650e3490f0cdf61ddac292d6a9a31e4b8ffb83f
murdex5 <fkadupitige@gmail.com>
2023-11-15 14:29:11 +0100

    added posts model added post creation"

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+    post_id = models.AutoField(primary_key=True)
+    post_author = models.ForeignKey(User, on_delete=models.CASCADE, default=User.objects.first().id)
+    
+    ## Post content
+    post_title = models.CharField(max_length=1000)
+    post_created = models.DateField(auto_now_add=True)
+    post_updated = models.DateField(auto_now=True)
+    # post_image = models.ImageField(upload_to="uploads/")
+   
+    def __str__(self):
+        return self.post_title
